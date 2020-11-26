3ae3fd7cbdc37dc3b4c184bed29284aa399c2a6d
Taavi Mustajoki <taavi.mustajoki@aalto.fi>
2020-11-26 02:32:11 +0200

    Add more code

---
+++ @app/client/src/pages/create-event/index.tsx
@@ -0,0 +1,142 @@
+import { ApolloError } from "@apollo/client";
+import { AuthRestrict, Redirect, SharedLayout } from "@app/components";
+import {
+  CreatedEventFragment,
+  useCreateEventMutation,
+  useSharedQuery,
+} from "@app/graphql";
+import { formItemLayout, getCodeFromError, tailFormItemLayout } from "@app/lib";
+import { Alert, Button, Col, Form, Input, PageHeader, Row, Select } from "antd";
+import { NextPage } from "next";
+import { Store } from "rc-field-form/lib/interface";
+import React, { useCallback, useState } from "react";
+
+const { Option } = Select;
+
+const CreateOrganizationPage: NextPage = () => {
+  const [formError, setFormError] = useState<Error | ApolloError | null>(null);
+  const query = useSharedQuery();
+  const [form] = Form.useForm();
+
+  const code = getCodeFromError(formError);
+  const [event, setEvent] = useState<null | CreatedEventFragment>(null);
+  const [createEvent] = useCreateEventMutation();
+  const handleSubmit = useCallback(
+    async (values: Store) => {
+      setFormError(null);
+      try {
+        const { name, description, organization } = values;
+        const { data } = await createEvent({
+          variables: {
+            name,
+            desc: description,
+            org_id: organization,
+          },
+        });
+        setFormError(null);
+        setEvent(data?.createEvent?.event || null);
+      } catch (e) {
+        setFormError(e);
+      }
+    },
+    [createEvent]
+  );
+
+  if (event) {
+    return <Redirect layout href="/" />;
+  }
+
+  if (
+    !query.data?.currentUser?.organizationMemberships.nodes.length ||
+    query.data?.currentUser?.organizationMemberships.nodes.length <= 0
+  ) {
+    return <Redirect layout href="/" />;
+  }
+
+  return (
+    <SharedLayout title="" query={query} forbidWhen={AuthRestrict.LOGGED_OUT}>
+      <Row>
+        <Col flex={1}>
+          <PageHeader title="Create Event" />
+          <div>
+            <Form {...formItemLayout} form={form} onFinish={handleSubmit}>
+              <Form.Item
+                label="Organization"
+                name="organization"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please choose an organizer for the event",
+                  },
+                ]}
+              >
+                <Select
+                  placeholder="Please select an organizer for the event"
+                  data-cy="createevent-select-organization-id"
+                >
+                  {query.data?.currentUser?.organizationMemberships.nodes.map(
+                    (a) => (
+                      <Option value={a.organization?.id}>
+                        {a.organization?.name}
+                      </Option>
+                    )
+                  )}
+                </Select>
+              </Form.Item>
+              <Form.Item
+                label="Name"
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please choose a name for the event",
+                  },
+                ]}
+              >
+                <Input data-cy="createevent-input-name" />
+              </Form.Item>
+              <Form.Item
+                label="Description"
+                name="description"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please tell something about the event",
+                  },
+                ]}
+              >
+                <Input.TextArea data-cy="createevent-input-description" />
+              </Form.Item>
+              {formError ? (
+                <Form.Item {...tailFormItemLayout}>
+                  <Alert
+                    type="error"
+                    message={`Creating event failed`}
+                    description={
+                      <span>
+                        extractError(formError).message
+                        {code ? (
+                          <span>
+                            {" "}
+                            (Error code: <code>ERR_{code}</code>)
+                          </span>
+                        ) : null}
+                      </span>
+                    }
+                  />
+                </Form.Item>
+              ) : null}
+              <Form.Item {...tailFormItemLayout}>
+                <Button htmlType="submit" data-cy="createevent-button-create">
+                  Create
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </SharedLayout>
+  );
+};
+
+export default CreateOrganizationPage;
