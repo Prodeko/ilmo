5a4d6e2d8b9ed3af027f7f6933687c785f33f318
Timo Riski <25483483+rriski@users.noreply.github.com>
2020-12-22 01:55:18 +0200

    Don't use CSP in dev
    - Packages updates broke this too, this fixes dev

--- @app/server/src/middleware/installHelmet.ts
+++ @app/server/src/middleware/installHelmet.ts
@@ -1,6 +1,21 @@
 import { Express } from "express";
 import helmet from "helmet";
 
+const isDev = process.env.NODE_ENV === "development";
+
 export default function installHelmet(app: Express) {
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: isDev
+        ? false
+        : {
+            directives: {
+              defaultSrc: ["'self'"],
+              scriptSrc: ["'self'", "prodeko.org", "google.com"],
+              objectSrc: ["'none'"],
+              upgradeInsecureRequests: [],
+            },
+          },
+    })
+  );
 }
