05f9f5451033c12e895f5660b1ea00f267c285de
Timo Riski <25483483+rriski@users.noreply.github.com>
2020-12-30 03:29:53 +0200

    Clarify comments in helmet

--- @app/server/src/middleware/installHelmet.ts
+++ @app/server/src/middleware/installHelmet.ts
@@ -3,19 +3,28 @@ import helmet from "helmet";
 
 const isDev = process.env.NODE_ENV === "development";
 
+// Postgraphile v4.11.0 had some changes related to websockets
+// and Postgraphiql and referrer policy is required
+const commonPolicies = { referrerPolicy: { policy: "same-origin" } };
+
 export default function installHelmet(app: Express) {
   app.use(
-    helmet({
-      referrerPolicy: { policy: "same-origin" },
-      contentSecurityPolicy: isDev
-        ? false
-        : {
-            directives: {
-              defaultSrc: ["'self'"],
-              scriptSrc: ["'self'", "prodeko.org", "google.com"],
-              objectSrc: ["'none'"],
+    helmet(
+      isDev
+        ? {
+            // Dev needs 'unsafe-eval' due to
+            // https://github.com/vercel/next.js/issues/14221
+            contentSecurityPolicy: {
+              directives: {
+                ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+                "script-src": ["'self'", "'unsafe-eval'"],
+              },
             },
-          },
-    })
+            ...commonPolicies,
+          }
+        : {
+            ...commonPolicies,
+          }
+    )
   );
 }
