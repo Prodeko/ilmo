cf1639c69a25fd635704ba02bfee3cb529301677
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-01-06 00:49:21 +0200

    Lint

--- @app/worker/src/tasks/registration_complete__delete_rate_limit_key.ts
+++ @app/worker/src/tasks/registration_complete__delete_rate_limit_key.ts
@@ -1,5 +1,6 @@
 import { Task } from "graphile-worker";
 
+// eslint-disable-next-line
 const task: Task = async (rawPayload, { withPgClient }) => {
   // TODO: Delete rate limit key from redis
   // call this from function app_public.create_registration
