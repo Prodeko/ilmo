d8c8fb44bef2231e2a31b33daf6311d769c05f26
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-02-22 00:09:05 +0200

    Fix e2e tests

--- @app/e2e/cypress/integration/create_event.spec.ts
+++ @app/e2e/cypress/integration/create_event.spec.ts
@@ -16,25 +16,28 @@ context("Create event", () => {
 
     // Action
     cy.get("@createEventDataResult").then(() => {
-      cy.visit(Cypress.env("ROOT_URL") + "/create-event");
-      cy.getCy("createevent-select-language").click();
-      cy.getCy("createevent-select-language-option-en").click();
-      cy.getCy("createevent-select-language").click();
+      cy.visit(Cypress.env("ROOT_URL") + "/event/create");
+      cy.getCy("eventform-select-language").click();
+      // Both languages are selected by default. Click twice to make sure
+      // the select works.
+      cy.getCy("eventform-select-language-option-en").click();
+      cy.getCy("eventform-select-language-option-en").click();
+      cy.getCy("eventform-select-language").click();
 
-      cy.getCy("createevent-select-organization-id").click();
-      cy.getCy("createevent-select-organization-id-option-0").click();
+      cy.getCy("eventform-select-organization-id").click();
+      cy.getCy("eventform-select-organization-id-option-0").click();
 
-      cy.getCy("createevent-select-category-id").click();
-      cy.getCy("createevent-select-category-id-option-0").click();
+      cy.getCy("eventform-select-category-id").click();
+      cy.getCy("eventform-select-category-id-option-0").click();
 
-      cy.getCy("createevent-input-name-fi").type("Testitapahtuma");
-      cy.getCy("createevent-input-name-en").type("Test event");
+      cy.getCy("eventform-input-name-fi").type("Testitapahtuma");
+      cy.getCy("eventform-input-name-en").type("Test event");
 
-      cy.getCy("createevent-input-description-fi").type("Testikuvaus");
-      cy.getCy("createevent-input-description-en").type("Test description");
+      cy.getCy("eventform-input-description-fi").type("Testikuvaus");
+      cy.getCy("eventform-input-description-en").type("Test description");
 
-      cy.getCy("createevent-input-rangepicker").eq(0).click();
-      cy.getCy("createevent-input-rangepicker").within(() => {
+      cy.getCy("eventform-input-event-time").eq(0).click();
+      cy.getCy("eventform-input-event-time").within(() => {
         cy.get("input[id='eventTime']").type("2021-02-08 01:58", {
           force: true,
         });
@@ -42,12 +45,21 @@ context("Create event", () => {
       });
       cy.get(".ant-picker-footer button").click();
 
-      cy.getCy("createevent-switch-highlight").click();
+      cy.getCy("eventform-input-registration-time").eq(0).click();
+      cy.getCy("eventform-input-registration-time").within(() => {
+        cy.get("input[id='registrationTime']").type("2021-02-08 00:58", {
+          force: true,
+        });
+        cy.get("input").eq(1).click().type("2021-02-08 00:59", { force: true });
+      });
+      cy.get(".ant-picker-footer button").eq(1).click();
 
-      cy.getCy("createevent-header-image-upload").attachFile("kitten.jpg");
+      cy.getCy("eventform-switch-highlight").click();
+
+      cy.getCy("eventform-header-image-upload").attachFile("kitten.jpg");
       cy.get(".antd-img-crop-modal button").contains("OK").click();
 
-      cy.getCy("createevent-button-create").click();
+      cy.getCy("eventform-button-submit").click();
     });
 
     // Assertion
