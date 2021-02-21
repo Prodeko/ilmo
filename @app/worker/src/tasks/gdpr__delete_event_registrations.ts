80d2b69006e65a407298cdbb9f0b562353156864
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-02-22 00:37:53 +0200

    Add cron capability graphile worker
    - Add gdpr__delete_event_registrations.ts task which cleans up event registrations for events that are older than 60 days.

---
+++ @app/worker/src/tasks/gdpr__delete_event_registrations.ts
@@ -0,0 +1,31 @@
+import { Task } from "graphile-worker";
+
+// 30 days
+const GDPR_ANONYMIZATION_TIMEOUT = 60;
+
+const task: Task = async (_payload, { withPgClient }) => {
+  try {
+    await withPgClient(async (client) => {
+      // Delete event registrations after GDPR_ANONYMIZATION_TIMEOUT
+      await client.query({
+        text: `
+        with deleted_registrations as (
+          delete from app_public.registrations as r
+            using
+              app_public.events as e
+            where
+              r.event_id = e.id and
+              age(current_timestamp, e.event_end_time) > interval '1 day' * $1
+            returning *)
+        select count(*) from deleted_registrations
+      `,
+        values: [GDPR_ANONYMIZATION_TIMEOUT],
+        rowMode: "array",
+      });
+    });
+  } catch (e) {
+    throw e;
+  }
+};
+
+module.exports = task;
