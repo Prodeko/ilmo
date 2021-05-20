1f315ddfcf751d06e376728847a9aba7b138b529
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-05-21 01:25:17 +0300

    fix: clean persistent operations on each write

--- @app/graphql/addToPersistedOperations.js
+++ @app/graphql/addToPersistedOperations.js
@@ -2,9 +2,12 @@ const map = require("./server.json")
 const { promises: fsp } = require("fs")
 
 async function main() {
+  const persistedOperationsDir = `${__dirname}/.persisted_operations/`
+  await fsp.rmdir(persistedOperationsDir, { recursive: true })
+  await fsp.mkdir(persistedOperationsDir)
   await Promise.all(
     Object.entries(map).map(([hash, query]) =>
-      fsp.writeFile(`${__dirname}/.persisted_operations/${hash}.graphql`, query)
+      fsp.writeFile(`${persistedOperationsDir}/${hash}.graphql`, query)
     )
   )
 }
