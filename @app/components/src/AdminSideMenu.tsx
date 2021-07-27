838eae0d2cb27b7f69ee413d66b9f562481ceba4
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-07-27 14:44:40 +0300

    fix: simplify AdminSideMenu

--- @app/components/src/AdminSideMenu.tsx
+++ @app/components/src/AdminSideMenu.tsx
@@ -1,8 +1,7 @@
-import { ReactNode, useState } from "react"
+import { Key, ReactNode, useState } from "react"
 import { Menu, Typography } from "antd"
 import SubMenu from "antd/lib/menu/SubMenu"
 import { TextProps } from "antd/lib/typography/Text"
-import { difference } from "lodash"
 import Link from "next/link"
 
 import { Warn } from "."
@@ -19,56 +18,39 @@ export interface MenuItem {
   icon?: ReactNode
 }
 
+function isString(x: any): x is string {
+  return typeof x === "string"
+}
+
 const getMenuItem = (
   item: MenuItem,
-  initialKey: string
+  initialKey: Key
 ): [JSX.Element, string[]] => {
-  if (typeof item.target === "string") {
+  const { showWarn, titleProps, title, key, cy, icon, target } = item
+  if (isString(target)) {
     const inner = (
       <a>
-        <Warn okay={!item.showWarn}>
-          <Text {...item.titleProps}>{item.title}</Text>
+        <Warn okay={!showWarn}>
+          <Text {...titleProps}>{title}</Text>
         </Warn>
       </a>
     )
-    const initialKeyPath = item.key === initialKey ? [initialKey] : []
+    const initialKeyPath = key === initialKey ? [initialKey] : []
     return [
-      <Menu.Item key={item.key} data-cy={item.cy} icon={item.icon}>
-        <Link href={item.target}>{inner}</Link>
+      <Menu.Item key={key} data-cy={cy} icon={icon}>
+        <Link href={target}>{inner}</Link>
       </Menu.Item>,
       initialKeyPath,
     ]
   }
-  const children = item?.target?.map((i) => getMenuItem(i, initialKey))
+  const children = target?.map((i) => getMenuItem(i, initialKey))
   const keyPath = children?.find((i) => i[1].length > 0)
   return [
-    <SubMenu
-      key={item.key}
-      data-cy={item.cy}
-      icon={item.icon}
-      title={item.title}
-    >
+    <SubMenu key={key} data-cy={cy} icon={icon} title={title}>
       {children?.map((a) => a[0])}
     </SubMenu>,
-    keyPath ? keyPath[1].concat(item.key) : [],
+    keyPath ? keyPath[1].concat(key) : [],
   ]
-}
-
-const getKeyPeers = (key: string, items: MenuItem[]): string[] => {
-  if (items.some((item) => item.key === key)) {
-    return items.map((item) => item.key).filter((itemKey) => itemKey !== key)
-  }
-  const itemsWithChildren = items.filter(
-    (item) =>
-      typeof item.target !== "string" && typeof item.target !== "function"
-  )
-  for (let i = 0; i < itemsWithChildren.length; i++) {
-    const res = getKeyPeers(key, itemsWithChildren[i].target as MenuItem[])
-    if (res.length > 0) {
-      return res
-    }
-  }
-  return []
 }
 
 type MenuProps = {
@@ -79,28 +61,16 @@ type MenuProps = {
 export const AdminSideMenu = ({ items, initialKey }: MenuProps) => {
   const [openKeys, setOpenKeys] = useState<string[]>([])
 
+  const rootSubmenuKeys = items.map((item) => item.key)
   const menuItems = items.map((item) => getMenuItem(item, initialKey))
   const inner = menuItems.map((item) => item[0])
-  const initialOpenKeys = menuItems.find((i) => i[1].length > 0)
 
-  if (openKeys.length === 0) {
-    if (initialOpenKeys && initialOpenKeys[1].length > 0) {
-      setOpenKeys(initialOpenKeys[1])
-    }
-  }
-
-  const onOpenChange = (prekeys: React.ReactText[]) => {
-    const keys = prekeys.map((i) => i + "")
-    if (keys.length < openKeys.length) {
+  const onOpenChange = (keys: string[]) => {
+    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1)
+    if (rootSubmenuKeys.indexOf(latestOpenKey!) === -1) {
       setOpenKeys(keys)
     } else {
-      const newKeys = difference(keys, openKeys)
-      if (newKeys.length !== 1) {
-        throw new Error("Invalid AdminSideMenu item key")
-      }
-      const newKey = newKeys[0]
-      const keyPeers = getKeyPeers(newKey, items)
-      setOpenKeys(difference(keys, keyPeers))
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : [])
     }
   }
 
@@ -109,6 +79,7 @@ export const AdminSideMenu = ({ items, initialKey }: MenuProps) => {
       mode="inline"
       openKeys={openKeys}
       selectedKeys={[initialKey]}
+      // @ts-ignore
       onOpenChange={onOpenChange}
     >
       {inner}
