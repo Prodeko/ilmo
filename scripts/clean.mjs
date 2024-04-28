846c2b012ffed746da0ce9605100c312911c64c3
Timo Riski <github@timoriski.fi>
2024-04-28 11:50:42 +0300

    fix: clean script after rimraf upgrade

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -1,19 +1,22 @@
 #!/usr/bin/env node
-import rimraf from "rimraf"
 import { execSync } from "child_process"
+
+import { rimrafSync } from "rimraf"
+
 import { dirname } from "./_setup_utils.mjs"
 
 const __dirname = dirname(import.meta)
+const opts = { glob: true }
 
 try {
-  rimraf.sync(`${__dirname}/../@app/*/dist`)
-  rimraf.sync(`${__dirname}/../@app/*/tsconfig.tsbuildinfo`)
-  rimraf.sync(`${__dirname}/../@app/client/.next`)
-  rimraf.sync(`${__dirname}/../@app/server/uploads/*`)
-  rimraf.sync(`${__dirname}/../@app/graphql/index.*`)
-  rimraf.sync(`${__dirname}/../@app/graphql/introspection.json`)
-  rimraf.sync(`${__dirname}/../@app/graphql/introspection.min.json`)
-  rimraf.sync(`${__dirname}/../@app/e2e/cypress/downloads/*`)
+  rimrafSync(`${__dirname}/../@app/*/dist`, opts)
+  rimrafSync(`${__dirname}/../@app/*/tsconfig.tsbuildinfo`, opts)
+  rimrafSync(`${__dirname}/../@app/client/.next`, opts)
+  rimrafSync(`${__dirname}/../@app/server/uploads/*`, opts)
+  rimrafSync(`${__dirname}/../@app/graphql/index.*`, opts)
+  rimrafSync(`${__dirname}/../@app/graphql/introspection.json`, opts)
+  rimrafSync(`${__dirname}/../@app/graphql/introspection.min.json`, opts)
+  rimrafSync(`${__dirname}/../@app/e2e/cypress/downloads/*`, opts)
   execSync("nx clear-cache", { stdio: "inherit" })
 } catch (e) {
   console.error("Failed to clean up, perhaps rimraf isn't installed?")
